347e015fa7330a3704b1d5ab2334a391fe45ef46
Ksenia Agalakova <agalaksu111@gmail.com>
2023-04-19 20:01:19 +0300

    Added font-link and edited navbar.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
     return (
         <div className='navbar'>
             <div className='nav-container'>
-                <span className='logo'>Bookit</span>
+                <span className='logo font-link'>Bookit</span>
                 <div className="nav-items">
                     <button className="nav-button">Register</button>
-                    <button className="nav-button">Login</button>
+                    <button className="nav-button">Sign in</button>
                 </div>
             </div>
         </div>
