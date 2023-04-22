ee0abf885e7b8ec4828a3e928cfcd33e8e86c747
Ksenia Agalakova <agalaksu111@gmail.com>
2023-04-22 17:07:43 +0300

    Added responsive design for homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,28 @@
-import Footer from '../components/Footer'
-import FrontPage from '../components/FrontPage'
-import Header from '../components/Header'
-import MailList from '../components/MailList'
-import Navbar from '../components/Navbar'
-import PopularProperties from '../components/PopularProperties'
-import PropertyList from '../components/PropertyList'
-import './home.css'
+import Footer from "../components/Footer";
+import FrontPage from "../components/FrontPage";
+import Header from "../components/Header";
+import MailList from "../components/MailList";
+import Navbar from "../components/Navbar";
+import PopularProperties from "../components/PopularProperties";
+import PropertyList from "../components/PropertyList";
+import "./home.css";
 
 const Home = () => {
-    return (
-        <div>
-            <Navbar />
-            <Header />
-            <div className="home-container">
-                <FrontPage />
-                <h1 className="home-title">Browse by property type</h1>
-                <PropertyList />
-                <h1 className="home-title">Homes guests love</h1>
-                <PopularProperties />
-                <MailList />
-                <Footer />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <div className="home-container">
+        <FrontPage />
+        <h1 className="home-title">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="home-title">Homes guests love</h1>
+        <PopularProperties />
+        <MailList />
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
