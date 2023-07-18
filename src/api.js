148eb04b80dc091a0b96b11ed99574e18ed47778
Ksenia Agalakova <agalaksu111@gmail.com>
2023-07-18 20:36:14 +0300

    Add production API base URL

--- src/api.js
+++ src/api.js
@@ -1,3 +1,2 @@
-export const API_BASE_URL = (process.env.NODE_ENV !== 'production') ?
-    "http://localhost:8800" :
-    "";
+export const API_BASE_URL =
+  process.env.NODE_ENV !== "production" ? "http://localhost:8800" : "https://bookit-app.onrender.com";
