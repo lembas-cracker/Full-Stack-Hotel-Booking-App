aea3a85a949cd483d3c57748e4173dda69a39d5c
Ksenia Agalakova <agalaksu111@gmail.com>
2024-06-07 18:56:43 +0300

    Wrapped App in AuthContext Provider

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HotelList from "./pages/HotelList";
 import Hotel from "./pages/Hotel";
 import "./App.css";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/hotels" element={<HotelList />} />
         <Route path="/hotels/:id" element={<Hotel />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { SearchContextProvider } from "./context/SearchContext";
+//import { SearchContextProvider } from "./context/SearchContext";
 import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
